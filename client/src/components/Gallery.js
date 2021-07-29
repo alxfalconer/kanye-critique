cab64d616c5686dcba4cc0d012a145dedf48c227
Alexander <al.x.falconer@gmail.com>
2021-07-28 21:15:31 -0400

    updates

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import ReviewList from './ReviewList'
 
 function Gallery() {
 
@@ -10,7 +9,7 @@ function Gallery() {
   const [artworks, setData] = useState([]);
 
   const fetchArt = async () => {
-    const data = await fetch('https://api.artic.edu/api/v1/artworks?limit=1')
+    const data = await fetch('https://api.artic.edu/api/v1/artworks?limit=10')
     const artworks = await data.json()
     setData(artworks.data);  
   }
@@ -24,7 +23,6 @@ return (
       {artworks.map(data => (
         <h2 fetchArt={fetchArt} className="artworks" key={data.id}>
           <img id="image" src={`https://www.artic.edu/iiif/2/${data.image_id}/full/843,/0/default.jpg`} alt=""/>
-          <ReviewList />
           </h2>
       ))}
     </div>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,9 +4,7 @@ const Home = () => (
     <div>
         <br></br>
         <div id="viewerContainer">
-        <p>Abstract painter Cy Twombly (1928-2011) drew inspiration from poetry, gathering lines from disparate sources to fuel his process.</p>
-        <p>Here on POEMS FOR CY we ask users to do the opposite-- create poetry inspired by artworks themselves.</p>
-        <p>Users can build on eachother's lines, creating a confluence of varying styles, perspectives and influences.</p>
+        <p>Kanye Critiques</p>
         </div>
         <br></br>
         <br></br>
